b2ca77d406bdd0f12fa8182066dc1ae7183eda3c
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-11-13 10:48:14 -0800

    finished

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express"); 
 const hbs = require("express-handlebars")
-
 const app = express();
+const fs = require("fs");
+const path = require('path'); 
 
 app.engine('handlebars', hbs.engine({defaultLayout: 'main'}));
 app.set("view engine", "handlebars"); 
 app.set("views", "./views"); 
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static("img")); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public')); 
+app.use('/img', express.static(path.join(__dirname, "public/img"))); 
+app.use(express.json()); 
 
 const todos = [
   "clean office", 
   "finish app"
-]
+];
+
+const imageUrl = '/public/img/activist.jpg'; 
 
 app.get("/home", (req, res) => {
-  res.render("home"); 
-})
+  res.render('home', {imageUrl}); 
+});
 
 app.get("/", (req, res) => {
   res.render("homePage", {
@@ -28,21 +32,11 @@ app.get("/", (req, res) => {
 app.post('/todo', (req, res) => {
   todos.push(req.body.content)
   res.redirect('/')
-})
+});
 
 app.get("/download", (req, res) => {
-  res.download("./img/activist.jpg"); 
+  res.download("./public/img/activist.jpg"); 
 }); 
-
-
-
-
-
-
-
-
-
-
 
 const PORT = 3000; 
 
